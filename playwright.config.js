558ceef298ae1e69e378fa621ddec95c98a5dfe5
EgonBolton <43938777+EgonBolton@users.noreply.github.com>
2023-07-20 16:42:24 -0300

    add playwright + test

---
+++ playwright.config.js
@@ -0,0 +1,28 @@
+import { defineConfig, devices } from "@playwright/test";
+
+const config = defineConfig({
+  testDir: "./tests",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  reporter: "html",
+  timeout: 5000,
+  use: {
+    // baseURL: "localhost:5173",
+    trace: "on-first-retry",
+  },
+  projects: [
+    { name: "chromium", use: { ...devices["Desktop Chrome"] } },
+    { name: "firefox", use: { ...devices["Desktop Firefox"] } },
+    { name: "webkit", use: { ...devices["Desktop Safari"] } },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: 'http://localhost:5173',
+    reuseExistingServer: !process.env.CI,
+
+    
+  },
+});
+
+export default config;
